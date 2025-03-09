8dc74b1017b58b3dcf9283e8629ccb9c896af540
probird5 <probird5>
2025-03-09 16:03:48 -0400

    updated nvim keybindings

--- shared/nvim/lua/vim-options.lua
+++ shared/nvim/lua/vim-options.lua
@@ -11,3 +11,6 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>h', ':bprevious<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', ':bnext<CR>', { noremap = true, silent = true })
+
