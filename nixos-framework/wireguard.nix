410217a3c2cf86407a5e133db68b8eeeb3f0cab0
probird5 <52969604+probird5@users.noreply.github.com>
2025-11-15 08:52:02 -0500

    updated flake

--- nixos-framework/wireguard.nix
+++
@@ -1,8 +0,0 @@
-
-{
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
-  };
-  # Enable WireGuard
-  networking.wireguard.enable = true;
-}
