8bd48b0f49567fe0217c755a41d149d29169c5d8
probird5 <52969604+probird5@users.noreply.github.com>
2025-12-09 07:03:37 -0500

    porting my tmux config

---
+++ modules/tmux.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    plugins = with pkgs;
+      [
+        {
+          plugin = tmux-super-fingers;
+          extraConfig = "set -g @super-fingers-key f";
+        }
+        tmuxPlugins.better-mouse-mode
+      ];
+    extraConfig = ''
+    set -g mouse on
+
+    # Vim style pane selection
+    bind h select-pane -L
+    bind j select-pane -D 
+    bind k select-pane -U
+    bind l select-pane -R
+
+    # Start windows and panes at 1, not 0
+    set -g base-index 1
+    set -g pane-base-index 1
+    set-window-option -g pane-base-index 1
+    set-option -g renumber-windows on
+
+    # Use Alt-arrow keys without prefix key to switch panes
+    bind -n M-Left select-pane -L
+    bind -n M-Right select-pane -R
+    bind -n M-Up select-pane -U
+    bind -n M-Down select-pane -D
+
+    # Shift arrow to switch windows
+    bind -n S-Left  previous-window
+    bind -n S-Right next-window
+    '';
+  };
+}
