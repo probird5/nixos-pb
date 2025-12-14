5823adf513114067945edc074fcdbba5bbb402a7
probird5 <52969604+probird5@users.noreply.github.com>
2025-12-14 14:42:02 -0500

    updated nvidia

--- hosts/bayle/configuration.nix
+++ hosts/bayle/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
   ];
+    services.xserver.videoDrivers = ["nvidia"];
 
   ############################################################
   # System identity / locale
