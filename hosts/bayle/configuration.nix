b0277c98a1717e9910c969ca2a8267e798053277
probird5 <52969604+probird5@users.noreply.github.com>
2026-01-12 14:58:42 -0500

    updated the config for tuigreet

--- hosts/bayle/configuration.nix
+++ hosts/bayle/configuration.nix
@@ -108,7 +108,7 @@
     greetd = {
       enable = true;
       settings.default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
         user = "probird5";
       };
     };
